8bcf3a036b9df2b7b25e6ee44e84dfb71bd929d2
Nikhil Agrawal <nikhil.agrwl07@gmail.com>
2016-08-29 11:04:44 +0530

    adding multithreading projects

---
+++ src/Multithreading/WaitNotifyExample/Message.java
@@ -0,0 +1,21 @@
+package Multithreading.WaitNotifyExample;
+
+/**
+ * Created by nikhilagrawal on 10/08/16.
+ */
+public class Message {
+    private String msg;
+
+    public Message(String str){
+        this.msg=str;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String str) {
+        this.msg=str;
+    }
+
+}
---
+++ src/Multithreading/WaitNotifyExample/Waiter.java
@@ -0,0 +1,31 @@
+package Multithreading.WaitNotifyExample;
+
+/**
+ * Created by nikhilagrawal on 10/08/16.
+ */
+public class Waiter implements Runnable{
+
+    private Message msg;
+
+    public Waiter(Message m){
+        this.msg=m;
+    }
+
+    @Override
+    public void run() {
+        String name = Thread.currentThread().getName();
+        synchronized (msg) {
+            System.out.println("Inside waiter.........................");
+            try{
+                System.out.println(name+" waiting to get notified at time:"+System.currentTimeMillis());
+                msg.wait();
+            }catch(InterruptedException e){
+                e.printStackTrace();
+            }
+            System.out.println(name+" waiter thread got notified at time:"+System.currentTimeMillis());
+            //process the message now
+            System.out.println(name+" processed: "+msg.getMsg());
+        }
+    }
+
+}
